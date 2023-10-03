f8d4e0d8784a6c787841af546fdece5db5399a7b
Iamneo <techadmin@iamneo.ai>
2023-10-03 10:30:35 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/example/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.example.controller;
+
+import com.example.model.Product;
+import com.example.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
+}
---
+++ springapp/src/main/java/com/example/model/product.java
@@ -0,0 +1,10 @@
+package com.example.model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+    private int quantity;
+    
+    // Getters and setters
+}
